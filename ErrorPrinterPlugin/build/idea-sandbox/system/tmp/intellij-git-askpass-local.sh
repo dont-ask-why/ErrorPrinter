330a924c019aa689b450b16094584f18316c646b
dont-ask-why <marder.frettchen@gmail.com>
2021-08-18 23:09:26 +0200

    Add both out and err channel option for printer output as well as removed deprecated API functions.

--- ErrorPrinterPlugin/build/idea-sandbox/system/tmp/intellij-git-askpass-local.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-"/home/verobuckina/.gradle/caches/modules-2/files-2.1/com.jetbrains/jbre/jbr_jcef-11_0_11-linux-x64-b1504.13/extracted/jbr/bin/java" -cp "/home/verobuckina/.gradle/caches/modules-2/files-2.1/com.jetbrains.intellij.idea/ideaIC/2021.2/b0727ceddea2b62b16825db9308e14a470198e7f/ideaIC-2021.2/plugins/git4idea/lib/git4idea-rt.jar:/home/verobuckina/.gradle/caches/modules-2/files-2.1/com.jetbrains.intellij.idea/ideaIC/2021.2/b0727ceddea2b62b16825db9308e14a470198e7f/ideaIC-2021.2/lib/3rd-party.jar" git4idea.http.GitAskPassApp "$@"
